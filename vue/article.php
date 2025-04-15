7e8795a50784d656c4677f90586c42a7ff1842a5
Debeing <mbaitelameric@gmail.com>
2025-04-15 04:08:10 +0100

    modification du fichier article.php

--- vue/article.php
+++ vue/article.php
@@ -5,23 +5,23 @@ include'entete.php'
         <form action="../model/ajoutArticle.php" method="POST">
             <div class="contenu">
                 <div class="input">
-                    <label for="name">Nom de l'article</label>
-                    <input type="text" name="text" placeholder="Veuillez saisir le nom"><br>
-                    <label for="text">Catégorie</label><br>
-                    <select name="Eric" id="Eric">
-                        <option value="Eric">option1</option>
-                        <option value="Eric">option1</option>
-                        <option value="Eric">option1</option>
-                        <option value="Eric">option1</option>
+                    <label for="nom_article">Nom de l'article</label>
+                    <input type="text" name="nom_article" id="nom_article" placeholder="Veuillez saisir le nom"><br>
+                    <label for="categorie">Catégorie</label><br>
+                    <select name="categorie" id="Eric">
+                        <option value="Voiture">Voiture</option>
+                        <option value="pneu">pneu</option>
+                        <option value="chaisse">chaisse</option>
+                        <option value="moteur">moteur</option>
                     </select><br>
-                    <label for="number">Quantité</label><br>
-                    <input type="number" name="number" placeholder="Veuillez saisir la Quantité">
-                    <label for="Prix">Prix unitaire</label><br>
-                    <input type="number" name="Prix" placeholder="Veuillez saisir le Prix unitaire"><br>
-                    <label for="date">Date de fabrication</label><br>
-                    <input type="date" name="date"><br>
-                    <label for="date">Date d'expiration</label><br>
-                    <input type="date" name="date"><br>
+                    <label for="quantite">Quantité</label><br>
+                    <input type="number" name="quantite" placeholder="Veuillez saisir la Quantité">
+                    <label for="prix_unitaire">Prix unitaire</label><br>
+                    <input type="number" name="prix_unitaire" placeholder="Veuillez saisir le Prix unitaire"><br>
+                    <label for="date_fabrication">Date de fabrication</label><br>
+                    <input type="datetime-local" name="date_fabrication"><br>
+                    <label for="date_expiration">Date d'expiration</label><br>
+                    <input type="datetime-local" name="date_expiration"><br>
                     <button type="submit" class="submit">Validé</button>
                 </div>
             </div>
